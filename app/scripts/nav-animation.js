b373cded65dd47d8cd79d5342e4d433c4686a47c
Anthony Oberto <anthonyoberto@gmail.com>
2015-06-18 16:29:41 -0700

    added interactivity and animation. Skrollr, Classie and Modernizr js libraries added as well

---
+++ app/scripts/nav-animation.js
@@ -0,0 +1,55 @@
+var navAnimation = (function() {
+
+	var docElem = document.documentElement,
+		header = document.querySelector( '.primary-nav-wrapper' ),
+		didScroll = false,
+		changeHeaderOn = 46;
+
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 0 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( header, 'primary-nav-wrapper-shrink' );
+		}
+		else {
+			classie.remove( header, 'primary-nav-wrapper-shrink' );
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+})();
+
+var $root = $('html, body');
+
+$('a').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, {
+    	duration: 850,
+    	specialEasing: {
+    		height: "easeInOut"
+    	}
+    });
+    return false;
+});
+
+var a = 10;
+function doStuff() {
+  var a = "cats"; // ok, but don't!
+  console.log(a);
+}
+console.log(a);
